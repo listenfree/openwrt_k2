2219eb5edd24f42777c859751a127b8fefd347e9
listenfree <chowchi@gmail.com>
2020-01-05 22:31:36 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
- version="18.06.4"
+ version="19.07.0-rc2"
  wget https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
  xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
  tar -xvf openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar
  cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
- make image PROFILE=psg1218a PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap wpad -wpad-mini -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
+ make image PROFILE=psg1218a PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-mod-admin-full luci-theme-bootstrap wpad -wpad-basic -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
