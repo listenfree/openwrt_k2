7a2769a97e6fae0b3c9adf465fd1616db0ab2ec4
listenfree <chowchi@gmail.com>
2021-09-08 13:06:53 +0800

    Update compile.sh
    add wpa3

--- compile.sh
+++ compile.sh
@@ -5,4 +5,4 @@
  cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
  make info
  make image PROFILE=phicomm_psg1218a PACKAGES="uhttpd luci-base luci-app-shadowsocks-libev shadowsocks-libev-ss-local -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
- make image PROFILE=lenovo_newifi-y1s PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad luci-app-ddns -wpad-basic-wolfssl -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
+ make image PROFILE=lenovo_newifi-y1s PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp luci-mod-admin-full luci-theme-bootstrap wpad-openssl luci-app-ddns  -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only" CONFIG_IPV6=n
