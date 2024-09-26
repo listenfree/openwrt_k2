66309e55f561ec0192cf57e9a278b8fad83bbee5
listenfree <chowchi@gmail.com>
2024-09-26 11:21:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
  #good for you
-version="23.05.4"
+version="23.05.5"
 current_path = $(pwd)
 wget https://downloads.openwrt.org/releases/${version}/targets/ramips/mt7620/openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
 xz -d openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64.tar.xz
